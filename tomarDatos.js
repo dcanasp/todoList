c958e1f5402994eb032d90a6283293af89df782e
david canas <dcanasp@unal.edu.co>
2022-01-11 08:08:31 -0500

    basura

--- tomarDatos.js
+++ tomarDatos.js
@@ -4,31 +4,107 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
+    //res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Content-Type', 'text/html');
     const url = req.url;
     const method = req.method;
-    let body = conseguirBody(req).then(
-        ()=>{
-        console.log(body)
-        }
-    ); 
+    let bodyFinal    
+    const body = [];//array
+    req.on('data', (chunk) => {
+
+        //console.log(chunk);//da como resultsdo algo ilegible
+        body.push(chunk);//se puede hacer push asi sea cnt
+
+    })
+    req.on('end', () => {
+       bodyFinal = Buffer.concat(body).toString();//pasa chunk a algo legible
+       console.log(bodyFinal);
+      });
+    req.on("end", () => {
+        res.write("ojala")
+           
+        console.log(html);
+        res.write(html);
+        res.end();  
+    })
+
+    
+function crearHtml(req) {
+    var header = '';
+    var body = '';
+  
+    // concatenate header string
+    // concatenate body string
+  
+    return '<html><head>' + header + '</head><body>' + body + '</body></html>';
+  };
+    /*
+    req.on('data', (chunk) => {
+      });//req.on nos permite escuchar eventos, hay evento data, problem etc..
+        */     
+ 
+    
+
+
+
+
+
+    /*
+        let body =conseguirBody(req).then(
+            ()=>{
+            console.log("??");
+            console.log(body);
+            }
+        );
+        */
+    //let body = ayuda(req);
+    //console.log(body);
     //res.write(body);
-    res.write("prueba");
-    res.end();
+    //res.write("prueba");
+    //res.end();
 });
 
-
-const conseguirBody = async (req) => {
+const conseguirBody = (req) => {
+    /* nada funciona estoy triste
+    return (function(resolve, reject){
     const bodyRaw = [];//array
     let body;
-    let xx = await req.on('data', (chunk) => {
-    bodyRaw.push(chunk);//se puede hacer push asi sea cnt
-    return Buffer.concat(bodyRaw).toString();
-    });//req.on nos permite escuchar eventos, hay evento data, problem etc..
-    final = req.on('end', () => {
-        return xx;
-    });
-   
+    //req.on nos permite escuchar eventos, hay evento data, problem etc..
+    req.on('data', (chunk) => {
+        bodyRaw.push(chunk);
+        console.log(1);
+    })
+    }).then(() =>{
+        req.on('data', (chunk) => {
+        body =Buffer.concat(bodyRaw).toString();
+        console.log(2);    
+        });
+    }).then(() => {
+        if(body==undefined){
+            reject(Error("mevoyamatar"));
+
+        }
+        console.log(3);
+        resolve(body);
+    })
+    
+
+*/
+}
+const ayuda = (req) =>{
+    let done = true
+
+const isItDoneYet = new Promise((resolve, reject) => {
+  if (done) {
+    const workDone = 'Here is the thing I built'
+    resolve(workDone)
+  } else {
+    reject('falla').catch(err => {
+  console.error(err)
+})
+  }
+})
+return isItDoneYet;
 }
 
 
